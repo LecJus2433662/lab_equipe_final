308564a93262e4641e48b5acf5ff245bd6b689ed
LecJus2433662 <justinleclerc11@gmail.com>
2025-04-14 08:39:49 -0400

    le commencement

---
+++ lab_equipe_final/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_equipe_final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
