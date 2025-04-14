fd2c59c056cd80805e4997b868a722226ce8ad2f
TurAle2433991 <aturcotte2007@icloud.com>
2025-04-14 12:58:49 -0400

    commit

--- lab_equipe_final/Program.cs
+++ lab_equipe_final/Program.cs
@@ -10,39 +10,22 @@ namespace lab_equipe_final
 
             try
             {
-                Calculateur multiplicateur = new Calculateur();
-                double nombre1 = 0;
-                double nombre2 = 0;
-                Console.WriteLine("calculatrice a multiplication");
-                Console.WriteLine("entrez un nombre");
-                nombre1 = double.Parse(Console.ReadLine());
-                Console.WriteLine("entrez un autre nombre");
-                nombre2 = double.Parse(Console.ReadLine());
-                Console.WriteLine("le résultat de la multiplication est :");
-                Console.WriteLine(multiplicateur.Multiplication(nombre1, nombre2));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("ce n'est pas un nombre, donc pas de bras, pas de chocolat");
-            }
-
-
-            try
-            {
-
+                
                 Calculateur calculateur = new Calculateur();
-                double nombre1 = 0;
-                double nombre2 = 0;
+                int nombre1 = 0;
+                int nombre2 = 0;
                 Console.WriteLine("Entrez un premier nombre à diviser");
-                nombre1 = double.Parse(Console.ReadLine());
+                nombre1 = int.Parse(Console.ReadLine());
                 Console.WriteLine("Entrez un deuxième nombre à diviser");
-                nombre2 = double.Parse(Console.ReadLine());
+                nombre2 = int.Parse(Console.ReadLine());
                 calculateur.Division(nombre1, nombre2);
                 Console.WriteLine("Le resultat de la division est :");
                 Console.WriteLine(calculateur.Division(nombre1, nombre2));
+
             }
-            catch (Exception ex)
+            catch (DivideByZeroException)
             {
+                Console.WriteLine();
                 Console.WriteLine("T vrm guez utilise une calcu a place");
             }
             
