283c5b03945c7942d63a7069f39f086cdeddf012
LecJus2433662 <justinleclerc11@gmail.com>
2025-04-21 10:56:00 -0400

    commit

--- Agence/VaisseauVitesse.cs
+++ Agence/VaisseauVitesse.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Agence
 {
-    internal class VaisseauVitesse
+    public partial class Vaisseau
     {
+        public void ChangerVitesse(double nouvelleVitesse)
+        {
+            try
+            {
+                if (nouvelleVitesse > vitesseMax)
+                {
+                    throw new InvalidOperationException("La vitesse demandée dépasse la vitesse maximale du vaisseau");
+                }
+
+                vitesseActuelle = nouvelleVitesse;
+                Console.WriteLine($"Vitesse changée : {vitesseActuelle} km/h.");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Erreur de changement de vitesse : {ex.Message}");
+            }
+        }
     }
 }
