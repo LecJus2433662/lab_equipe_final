b43cc1a994570b7fd56531b539ed27bb044a1151
LecJus2433662 <justinleclerc11@gmail.com>
2025-04-21 10:28:07 -0400

    commit

--- Agence/Vaisseau.cs
+++ Agence/Vaisseau.cs
@@ -6,8 +6,40 @@ using System.Threading.Tasks;
 
 namespace Agence
 {
-    public class Vaisseau
+    public partial class Vaisseau
     {
+        public string nom { get; set; }
+        public int capacite { get; set; }
+        public double vitesseActuelle { get; set; }
+        public double vitesseMax { get; set; }
+        private bool enOrbite;
+        private List<Mission> missionsEnCours = new List<Mission>();
 
+
+        public Vaisseau(string nom, int capacite, int vitesseActuelle, int vitesseMax, bool enOrbite)
+        {
+            this.nom = nom;
+            this.capacite = capacite;
+            this.vitesseActuelle = vitesseActuelle;
+            this.vitesseMax = vitesseMax;
+            this.enOrbite = enOrbite;
+        }
+        public void Decollage(int x, int y,DateTime date)
+        {
+            try
+            {
+                if (enOrbite)
+                {
+                    throw new InvalidOperationException("Le vaisseau est déjà en orbite");
+                }
+                enOrbite = true;
+                enOrbite = true;
+                Console.WriteLine($"Décollage en orbite");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"erreur lors du décollage : {ex.Message}");
+            }
+        }
     }
 }
