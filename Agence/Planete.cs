20b939bd3b4d53af6e51efa8e3aea5cd3ba8479a
TurAle2433991 <aturcotte2007@icloud.com>
2025-04-21 11:21:09 -0400

    commit planete

--- Agence/Planete.cs
+++ Agence/Planete.cs
@@ -8,6 +8,41 @@ namespace Agence
 {
     public class Planete
     {
+        string nom { get; set; }
+        int taille { get; set; }
+        int distanceTerre { get; set; }
+        bool atmosphere { get; set; }
+        bool exploree { get; set; }
 
+        public Planete(string nom, int taille, int distanceTerre, bool atmosphere, bool explorer)
+        {
+            this.nom = nom;
+            this.taille = taille;
+            this.distanceTerre = distanceTerre;
+            this.atmosphere = atmosphere;
+            exploree = explorer;
+        }
+
+        public void Explorer()
+        {
+            try
+            {
+                if (exploree)
+                {
+                    throw new InvalidOperationException("Cette planète à déjà été explorer");
+                }
+                else
+                {
+                    exploree = true;
+                    string rapport = "";
+                    rapport += $"{nom} , {atmosphere}, {distanceTerre}, {taille}";
+                    Console.WriteLine(rapport);
+                }
+            }
+            catch(InvalidOperationException ex) 
+            {
+                Console.WriteLine($"Erreur lors de l'exploration de la planète : {ex.Message} ");
+            }
+        }
     }
 }
--- Agence/Program.cs
+++ Agence/Program.cs
@@ -6,7 +6,7 @@ namespace Agence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
